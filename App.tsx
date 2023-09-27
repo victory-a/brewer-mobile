6d8459ea0145c55901014f50acd40d90e8f6a327
victory-a <victoryasokomeh@gmail.com>
2023-09-27 04:11:14 +0100

    configure tailwind

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 
 function App() {
   return (
     <SafeAreaView>
-      <Text>Tex</Text>
+      <Text className="text-red-400">Text</Text>
     </SafeAreaView>
   );
 }
