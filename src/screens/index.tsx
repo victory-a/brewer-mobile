1fb44c44d383c47be017c2bad0088a458f03badc
victory-a <victoryasokomeh@gmail.com>
2023-09-27 03:14:18 +0100

    setup and test linter

---
+++ src/screens/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Index = () => {
+  console.log('');
+
+  return (
+    <View>
+      <Text>ppp</Text>
+    </View>
+  );
+};
+
+export default Index;
