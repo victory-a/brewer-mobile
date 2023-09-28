0f6912bc1b6c61b70fdb5f6c51e855415c088437
victory-a <victoryasokomeh@gmail.com>
2023-09-29 00:38:34 +0100

    splash screen ios, app icon android

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 function App() {
+  React.useEffect(() => {
+    // SplashScreen.hide();
+  }, []);
+
   return (
     <SafeAreaView>
-      <Text className="text-red-400">Text</Text>
+      <Text className="text-red-400">Holla Holla we popping bottles</Text>
     </SafeAreaView>
   );
 }
