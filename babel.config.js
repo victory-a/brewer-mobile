9aed603ea3d1cfd159786161d1d1639eb29e8bf4
victory-a <victoryasokomeh@gmail.com>
2023-10-04 10:34:13 +0100

    fix: eslint parser error

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['nativewind/babel']
+  plugins: [
+    'nativewind/babel',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          src: './src',
+          components: './src/components',
+          screens: './src/screens',
+          navigation: './src/navigation'
+        }
+      }
+    ]
+  ]
 };
