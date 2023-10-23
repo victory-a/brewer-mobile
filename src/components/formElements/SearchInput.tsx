10e34727553b774f5553a25eb58445a6ca29292c
victory-a <victoryasokomeh@gmail.com>
2023-10-24 00:30:42 +0100

    search screen

--- src/components/formElements/SearchInput.tsx
+++ src/components/formElements/SearchInput.tsx
@@ -5,10 +5,10 @@ import {} from '../Icons';
 
 const SearchInput = (props: TextInputProps) => {
   return (
-    <View className="rounded-md border border-white-color bg-[#F4F4F4]">
+    <View className="rounded-md border border-white-color bg-sea-shell pr-1">
       <TextInput
         {...props}
-        className="rounded-lg border-transparent bg-[#F4F4F4] px-2 py-[10px] font-Sora-regular text-[15px] text-mid-gray"
+        className="rounded-lg border-transparent bg-transparent px-2 py-[14] font-Sora-regular text-[15px] text-mid-gray"
         selectionColor={colors.primary}
         placeholderTextColor={colors['light-gray']}
         autoCapitalize="none"
--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { Keyboard, SafeAreaView, TouchableWithoutFeedback } from 'react-native';
 
 import { ContainerView, SearchInput } from 'src/components';
 
 const SearchScreen = () => {
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <ContainerView className="mt-3">
-        <SearchInput placeholder="Search for coffee brand" />
-      </ContainerView>
-    </SafeAreaView>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} className=" bg-red-300">
+      <SafeAreaView className="flex-1 bg-white">
+        <ContainerView className="mt-3 flex-1 ">
+          <SearchInput placeholder="Search for coffee brand" />
+        </ContainerView>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 };
 
