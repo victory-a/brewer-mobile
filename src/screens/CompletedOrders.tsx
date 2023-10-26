1b002565242768ce21c38b09df8f073e0e93a8d1
victory-a <victoryasokomeh@gmail.com>
2023-10-27 00:44:10 +0100

    wip: pull to refresh completed orders

--- src/screens/CompletedOrders.tsx
+++ src/screens/CompletedOrders.tsx
@@ -1,10 +1,11 @@
-import { SafeAreaView, ScrollView, View } from 'react-native';
+import { RefreshControl, SafeAreaView, ScrollView, View } from 'react-native';
 import React from 'react';
 import { FlashList } from '@shopify/flash-list';
 
 import { ContainerView, EmptyCart } from 'src/components';
 // import { useAppNavigation } from 'src/hooks/useTypedNavigation';
 import { OrderItem } from 'src/components/OrderItem';
+import { colors } from 'src/styles/theme';
 
 const dummyOrder = {
   orderCode: '1255334',
@@ -15,12 +16,31 @@ const completedOrders = Array.from({ length: 5 }, () => dummyOrder);
 
 export function CompletedOrders() {
   // const { navigate } = useAppNavigation();
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 5000);
+  }, []);
+
   return (
     <SafeAreaView className="flex-1 bg-white">
       <EmptyCart headline="We are waiting for your first order" />
 
       {/* {dummyOrder ? (
-        <ScrollView className="mt-5 px-2" showsVerticalScrollIndicator={false}>
+        <ScrollView
+          className="mt-5 px-2"
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+              tintColor={colors['white-color']}
+            />
+          }
+        >
           <View className="min-h-[2]">
             <FlashList
               data={completedOrders}
