21783c758961b63a7b719d5ffce4d576b82ed820
victory-a <victoryasokomeh@gmail.com>
2023-11-01 23:43:46 +0100

    keyboard dismiss

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, View } from 'react-native';
+import { Keyboard, SafeAreaView, TouchableWithoutFeedback, View } from 'react-native';
 
 import { ContainerView, PhoneInput, Text, TextInput } from 'src/components';
 import { SolidButton, TextButton } from 'src/components/formElements/Button';
@@ -9,28 +9,30 @@ import { useAuth } from 'src/context/AuthContext';
 const ProfileScreen = () => {
   const { setIsAuthenticated } = useAuth();
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <ContainerView className="flex-1 pt-8">
-        <TextInput placeholder="Adnan Frimpong" label="Name" value="" />
-        <TextInput value="pingpong98" label="Username" />
-        <PhoneInput value="812 345 6789" label="mobile" />
-        <TextInput value="adanan@example.com" label="Email" editable={false} />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} className=" bg-red-300">
+      <SafeAreaView className="flex-1 bg-white">
+        <ContainerView className="flex-1 pt-8">
+          <TextInput placeholder="Adnan Frimpong" label="Name" value="" />
+          <TextInput value="pingpong98" label="Username" />
+          <PhoneInput value="812 345 6789" label="mobile" />
+          <TextInput value="adanan@example.com" label="Email" editable={false} />
 
-        <SolidButton buttonclassName="mt-6">Save</SolidButton>
+          <SolidButton buttonclassName="mt-6">Save</SolidButton>
 
-        <TextButton
-          labelClassName="text-red-500"
-          buttonclassName="my-5"
-          onPress={() => setIsAuthenticated(false)}
-        >
-          Logout
-        </TextButton>
+          <TextButton
+            labelClassName="text-red-500"
+            buttonclassName="my-5"
+            onPress={() => setIsAuthenticated(false)}
+          >
+            Logout
+          </TextButton>
 
-        <View className="mb-3 mt-auto">
-          <Text className="text-center text-zambezi">v1.0.0</Text>
-        </View>
-      </ContainerView>
-    </SafeAreaView>
+          <View className="mb-3 mt-auto">
+            <Text className="text-center text-zambezi">v1.0.0</Text>
+          </View>
+        </ContainerView>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 };
 
