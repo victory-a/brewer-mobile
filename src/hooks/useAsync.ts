c6dc29783cbe46b62bfaa5325ce1391b419be003
victory-a <victoryasokomeh@gmail.com>
2023-11-14 02:23:33 +0100

    feat: useAsync

---
+++ src/hooks/useAsync.ts
@@ -0,0 +1,48 @@
+import { useState, useEffect, useCallback } from 'react';
+
+interface AsyncState<T> {
+  status: 'idle' | 'pending' | 'success' | 'error';
+  value: T | null;
+  error: Error | null;
+}
+
+interface UseAsync<T> {
+  execute: (props?: any) => Promise<void>;
+  status: AsyncState<T>['status'];
+  value: AsyncState<T>['value'];
+  error: AsyncState<T>['error'];
+}
+
+const useAsync = <T>(asyncFunction: (props?: any) => Promise<T>, immediate = true): UseAsync<T> => {
+  const [state, setState] = useState<AsyncState<T>>({
+    status: 'idle',
+    value: null,
+    error: null
+  });
+
+  const execute = useCallback(
+    (props = undefined): Promise<void> => {
+      setState({ status: 'pending', value: null, error: null });
+      return asyncFunction(props)
+        .then((response) => {
+          setState({ status: 'success', value: response, error: null });
+        })
+        .catch((error) => {
+          setState({ status: 'error', value: null, error });
+        });
+    },
+    [asyncFunction]
+  );
+
+  useEffect(() => {
+    if (immediate) {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      execute();
+    }
+  }, [execute, immediate]);
+
+  const { value, status, error } = state;
+  return { execute, value, status, error };
+};
+
+export default useAsync;
