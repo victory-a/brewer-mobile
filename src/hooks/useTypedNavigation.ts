b31644a01b050c9e07723af4969f039515f5aff1
victory-a <victoryasokomeh@gmail.com>
2023-11-14 02:24:57 +0100

    feat: navigation hooks

--- src/hooks/useTypedNavigation.ts
+++ src/hooks/useTypedNavigation.ts
@@ -1,8 +1,10 @@
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute, RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 
 import { AppNavigatorParams, AuthNavigatorParams } from 'src/model/navigation.model';
 
 export const useAuthNavigation = () => useNavigation<StackNavigationProp<AuthNavigatorParams>>();
-
 export const useAppNavigation = () => useNavigation<StackNavigationProp<AppNavigatorParams>>();
+
+export const useAuthNavigationRoute = () => useRoute<RouteProp<AuthNavigatorParams>>();
+export const useAppNavigationRoute = () => useRoute<RouteProp<AppNavigatorParams>>();
